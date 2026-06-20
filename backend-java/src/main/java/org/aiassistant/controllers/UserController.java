fa676542b3c01941680e897c9524ba0f1974685a
Manoj Kumar <manoj.kumar@autorabit.com>
2026-06-20 09:18:10 +0530

    initial project refactoring

--- backend-java/src/main/java/org/aiassistant/controllers/UserController.java
+++ backend-java/src/main/java/org/aiassistant/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/")
     public String helloUser() {
         return "Hi From Spring boot backend... Manoj...";
     }
